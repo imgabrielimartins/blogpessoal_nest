c345beb0f67a1e01e8ae58f060dbd97c6d33bc86
Gabrieli <gabrielidelimamartins@gmail.com>
2025-12-10 15:56:36 -0300

    feat: add metodos a classe usuario controller

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsuarioService } from '../services/usuario.service';
 import { Usuario } from '../entities/usuario.entity';
 
@@ -6,9 +16,27 @@ import { Usuario } from '../entities/usuario.entity';
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
+  @Get('/all')
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Usuario[]> {
+    return this.usuarioService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+    return this.usuarioService.findById(id);
+  }
+
   @Post('/cadastrar')
   @HttpCode(HttpStatus.CREATED)
   create(@Body() usuario: Usuario): Promise<Usuario | null> {
     return this.usuarioService.create(usuario);
+  }
+
+  @Put('/atualizar')
+  @HttpCode(HttpStatus.OK)
+  async update(@Body() usuario: Usuario): Promise<Usuario> {
+    return this.usuarioService.update(usuario);
   }
 }
