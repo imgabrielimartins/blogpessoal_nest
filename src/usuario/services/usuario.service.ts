a7283d123ef07976532597884b1490a97b4dcf38
Gabrieli <gabrielidelimamartins@gmail.com>
2025-12-10 15:46:03 -0300

    feat: add metodos a classe usuario service

--- src/usuario/services/usuario.service.ts
+++ src/usuario/services/usuario.service.ts
@@ -20,11 +20,45 @@ export class UsuarioService {
     });
   }
 
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find();
+  }
+
+  async findById(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!usuario)
+      throw new HttpException('Usuario não encontrado', HttpStatus.NOT_FOUND);
+
+    return usuario;
+  }
+
   async create(usuario: Usuario): Promise<Usuario> {
     const buscaUsuario = await this.findByUsuario(usuario.usuario);
 
     if (buscaUsuario)
       throw new HttpException('O usuario ja existe!', HttpStatus.BAD_REQUEST);
+
+    usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
+    return await this.usuarioRepository.save(usuario);
+
+    return usuario;
+  }
+
+  async update(usuario: Usuario): Promise<Usuario> {
+    await this.findById(usuario.id);
+
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+    if (buscaUsuario && buscaUsuario.id !== usuario.id)
+      throw new HttpException(
+        'Usuario (e-mail) já cadastrado!',
+        HttpStatus.BAD_REQUEST,
+      );
 
     usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
     return await this.usuarioRepository.save(usuario);
